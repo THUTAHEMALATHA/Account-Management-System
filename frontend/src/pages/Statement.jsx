9ee35f275a2594a62d1b5059a5022774fcff7a33
hemalatha <thutahemalatha@gmail.com>
2026-03-15 17:14:25 +0530

    fetch: app, main

---
+++ frontend/src/pages/Statement.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import API from "../api/axios";
+
+ function Statement(){
+    const[transcations, setTranscations]= useState([]);
+    useEffect(()=>{
+        const fetchData = async()=>{
+            const res = await API.get("/account/statement");
+            setTranscations(res.data);
+        };
+        fetchData();
+    },[]);
+
+    return(
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                {transcations.map((tx)=>(
+                    <tr 
+                    key={tx.id}
+                    style={{
+                        color:tx.transcations_type === "credit" ? "green" : "red",
+
+                    }}>
+                        <td>{new Date(tx.created_at).toLocaleDateString()}</td>
+                        <td>{tx.transcations_type}</td>
+                        <td>₹{tx.amount}</td>
+
+                    </tr>
+                ))}
+            </tbody>
+
+        </table>
+    )
+
+
+
+ }
+ export default Statement;
+ 
